23c1251f6be13a49ab6441b16e2f8d8e0c886177
unknown <chenning@youbanban.com>
2020-12-11 18:41:37 +0800

    2020-12-11

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  position: fixed;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  height: 49px;
+  text-align: center;
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{ active: isActive }"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+      path: String
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) ==-1;
+    },
+  },
+  methods: {
+    itemClick() {
+        // console.log(this.path)
+      this.$router.replace(this.path);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+}
+
+.active {
+  color: red;
+}
+</style>
